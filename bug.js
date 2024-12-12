b58a96e0f2771e53e83470e0cfbb68acef9a6d51
BugHunter-X <askzenai@gmail.com>
2024-12-12 18:38:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+```javascript
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      {/* This comment causes the error */}      
+    </div>
+  );
+}
+```
